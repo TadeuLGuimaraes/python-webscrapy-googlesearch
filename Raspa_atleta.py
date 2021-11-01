8225e86142cce5545823fdcf17420621aee3f266
TadeuLGuimaraes <lutalgui.lab02@gmail.com>
2021-11-01 15:22:13 -0300

    feat:alterado func. raspa url para enerica

--- Raspa_atleta.py
+++ Raspa_atleta.py
@@ -1,22 +1,4 @@
 #Este projeto tem como finalidade criar uma solução que recupere na WEB as informações esportivas de uma pessoa.
-"""
-#FASE 1 - Procurando na web - Utilizando a bibliotea googlesearch
-            #FASE 1.1
-              Filtros das pesquisas atraves do nome e cpfj
-              Validar os sites que o link não estão mais funcionando
-              Obter atraves da url:
-                    Os sites correlacionadosos tipos(esportes, buscadores de informações, juridicos, redes sociais, etc.)
-                    Pais
-                    Ano.
-                    Sites dos organizadores das provas
-            #FASE 1.2
-              Ler o HTML dos sites e extraindo as informações: inscrições, resultados de provas e etc.
-              Resultados, categoria, tempos, local das provas e etc.
-              Confirmar as informações obtidas da fase 1.1 (
-                Os sites correlacionadosos tipos(esportes, buscadores de informações, juridicos, redes sociais, etc.)
-                    Pais
-                    Ano.
-"""
 # bibliotecas utilizadas
 from googlesearch import search
 import urllib
@@ -27,8 +9,6 @@ class raspa_url:
         def separa_url(site1,ano,lista_url_sites_palabras_esporte):   #obtem o endereço absoluto do site e o pais
                 url_separada=[]
                 Informcao_url = {}
-                ano_corrente1=datetime.now()
-                ano_corrente = ano_corrente1.year
 
                 if "//" in site1:
                     pos_barrasd=site1.find("//")+2
@@ -37,31 +17,28 @@ class raspa_url:
                     if "/" in url_separada1:            # separa url do site
                         pos_barrass = url_separada1.find("/")
                         url_separad2 = url_separada1[0:pos_barrass]
-                        if ".com." in url_separad2:      #separa do site o pais
-                            pos_pais = url_separad2.find(".com.")+5
-                            url_pais = url_separad2[pos_pais:pos_barrass]
-                        else:
-                            url_pais="NA"
-                        while True:             #preocura ano na url
-                                    if str(ano) in site1:
-                                        ano_achado=str(ano)
-                                        break
-                                    ano+=1
-                                    if ano == ano_corrente:
-                                        ano_achado="NA"
-                                        break
-                        v_ret =False
-                        for n in range(0, len(lista_url_sites_palabras_esporte)): # procura esporte na url
-                            v_esporte = lista_url_sites_palabras_esporte[n].upper() in site1.upper()
-                            if v_esporte == True:
-                                esporte=lista_url_sites_palabras_esporte[n].upper()
-                                break
-                            else:
-                                esporte ="NA"
+# aqui
+                Informcao_url3 = [url_separad2]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palavras_pais, "pais")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palavras_cidades, "cidades")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palavras_uf, "uf")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palavras_tiparq, "tipo arquivo")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palabras_correlacionadas,"correlacionados")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palabras_esporte, "esporte")]
+                Informcao_url3 = Informcao_url3 + [raspa_url.separa_url_generico(resultado, lista_url_sites_palavras_ano, "ano")]
+                Informcao_url_lista2.append(Informcao_url3)
 
-                Informcao_url = [url_separad2, url_pais, ano_achado,esporte]
-                Informcao_url_lista.append(Informcao_url)           # incl dicionario na lista
-
+        def separa_url_generico(site1, lista,tipo):
+            V_return = "NA"
+#            print(f" {tipo} ",end="")
+            for n in range(0, len(lista)):  # procura esporte na url
+                v_achado = str(lista[n]).upper() in site1.upper()
+                if v_achado == True:
+                    achado = lista[n].upper()
+                    V_return=achado
+                    break
+            return V_return
+            #print(f"Func achado {tipo}- > {achado}")
 class valida_site:
         def tipo(site1):
             """
@@ -99,32 +76,50 @@ class valida_site:
 # trata os dados
 # mostra os dados
 nome=str(input("Digite um nome completo - > ")).upper().strip()
-ano=1999
-teste=0
+print(f"Consultando o nome {nome}")
+ano=1942
 cont=0
 contotal=0
 listacerta= []
 listaerro=[]
 url_separada=[]
+#Informcao_url3=[]
 Informcao_url_lista=[]
+Informcao_url_lista2=[]
+lista_url_sites_palavras_pais=["BR"]
+lista_url_sites_palavras_cidades = ["PraiaGrande","Santos","SãoPaulo","\RIO","Riojaneiro","Riodejaneiro"]
+lista_url_sites_palavras_uf = ["\SP","_sp","_rj","\RJ"]
+lista_url_sites_palavras_tiparq=["/imagem/","/doc/"]
+ano_corrente1 = datetime.now()
+ano_corrente = ano_corrente1.year
+lista_url_sites_palavras_ano=[]
+lista_ano=[]
+while True:             #MONTA A LISTA DE ANOS
+      lista_ano = lista_ano+[str(ano)]
+      if ano == ano_corrente:
+            lista_url_sites_palavras_ano.append(lista_ano)
+            break
+      ano+=1
 
-lista_url_sites_palabras_ok = ["resultados", "corrida", "natação"
-    , "bike", "ciclismo", "maratona", "triathlon"
-    , "biatlhon", "duatlhom", "runner"
-    , "esporte", "meia", "esportiva", "marathon"
-    , "etapa", "atletas", "atleta", "basquete"
-    , "premiação", "AQUATHLON","futebol"]
+lista_url_sites_palabras_correlacionadas = ["resultados", "resultado"
+    , "esporte" , "esportes"
+    , "esportiva" , "esportivas"
+    , "etapa", "etapas"
+    , "atletas", "atleta"
+    , "premiação", "premiações"
+    , "endurance"]
 
 lista_url_sites_palabras_esporte = ["corrida", "natação"
     , "bike", "ciclismo", "maratona", "triathlon"
     , "biatlhon", "duatlhom", "runner", "meia", "marathon"
-    ,"basquete", "premiação", "AQUATHLON","futebol"]
+    ,"basquete", "AQUATHLON","futebol","Ironman", "swimming"]
 
+lista_url_sites_palabras_ok = lista_url_sites_palabras_correlacionadas+lista_url_sites_palabras_esporte
 for resultado in search('"'+nome+'"',
                         tld = 'com', lang = 'en',num = 10,
                         start = 0, stop = None, pause = 2.0):
     if valida_site.tipo(resultado): 
-        print(resultado)
+        print("\n"+resultado)
         valida_site.existe(resultado) 
         print("*" * 40)
         listacerta.append(resultado)
@@ -139,12 +134,49 @@ print(f"                                                    {contotal+cont}")
 print("-"*40)
 print(f"Qtd de sites correlacioandos com \033[31merro de acesso \033[m -> {len(listaerro)}")
 print(f"Qtd de sites correlacioandos com \033[32macesso ok      \033[m -> {cont-len(listaerro)}")
-
+print("<<< URL >>>")
 print("#"*40)
+
 for n in range(0,len(listacerta)):
     print(f" {n+1} - {listacerta[n]}")
+    print(f"{Informcao_url_lista2[n]}")
 print("#"*40)
-for n1 in range(0,len(url_separada)):
-    print(f" {n1 + 1} - {url_separada[n1]}")
-for n1 in range(0,len(Informcao_url_lista)):
-    print(f" {n1 + 1} - {Informcao_url_lista[n1]}")
+"""
+                ano_corrente1=datetime.now()
+                ano_corrente = ano_corrente1.year
+
+                        if ".com." in url_separad2:      #separa do site o pais
+                            pos_pais = url_separad2.find(".com.")+5
+                            url_pais = url_separad2[pos_pais:pos_barrass]
+                        else:
+                            url_pais="NA"
+                        while True:             #preocura ano na url
+                                    if str(ano) in site1:
+                                        ano_achado=str(ano)
+                                        break
+                                    ano+=1
+                                    if ano == ano_corrente:
+                                        ano_achado="NA"
+                                        break
+                        v_ret =False
+                        for n in range(0, len(lista_url_sites_palabras_esporte)): # procura esporte na url
+                            v_esporte = lista_url_sites_palabras_esporte[n].upper() in site1.upper()
+                            if v_esporte == True:
+                                esporte=lista_url_sites_palabras_esporte[n].upper()
+                                break
+                            else:
+                                esporte ="NA"
+             Informcao_url = [url_separad2, url_pais, ano_achado,esporte]
+             Informcao_url_lista.append(Informcao_url)           # incl dicionario na lista
+
+"""
+
+"""
+lista_url_sites_palavras_ano=["1964","1965","1966","1967","1968","1969"
+                                 ,"1970","1971","1972","1973","1974","1975","1976","1977","1978","1979"
+                                 ,"1981","1982","1983","1984","1985","1986","1987","1988","1989"
+                                ,"1990","1991","1992","1993","1994","1995","1996","1997","1998","1999"
+                                , "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"
+                                , "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"
+                                , "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030" ]
+"""
